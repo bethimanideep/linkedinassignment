5f17a9b292065c2d3c04c88b4a3372090647840e
bethimanideep <bethimanideep1999@gmail.com>
2023-02-20 21:07:52 +0530

    up

--- routes/route.js
+++ routes/route.js
@@ -44,7 +44,6 @@ router.post("/users/posts",async(req,res)=>{
     let current = await model.findOne({ name })
     if(current){
         res.json(current.posts)
-        console.log(current.posts);
     }
 })
 router.get("/posts/top",async(req,res)=>{
@@ -52,13 +51,47 @@ router.get("/posts/top",async(req,res)=>{
     res.json()
 })
 router.post("/posts/update",async(req,res)=>{
-    let data=await model.find()
-    res.json()
+    let name=req.body.name
+    let key=req.body.key
+    let input=req.body.input
+    let data=await model.findOne({name})
+    let x=data.posts
+    for(let i=0;i<x.length;i++){
+        if(req.body.index===i){
+            // x[i].key=input
+            if(key==="title"){
+                x[i].title=input
+            }else if(key=="name"){
+                x[i].name=input
+            }else if(key=="comment"){
+                x[i].comment=input
+            }else if(key=="device"){
+                x[i].device=input
+            }
+        }
+    }
+    await model.findOneAndUpdate({name},{posts:x})
+    let current = await model.findOne({ name })
+    if(current){
+        res.json(current.posts)
+    }
 })
 router.post("/posts/delete",async(req,res)=>{
     let name=req.body.name
-    let data=await model.findOneAndDelete({name})
-    res.json("deleted")
+    let data=await model.findOne({name})
+    let x=data.posts
+    let updatedarray=[]
+    for(let i=0;i<x.length;i++){
+        if(req.body.index!=i){
+            updatedarray.push(x[i])
+        }
+    }
+    await model.findOneAndUpdate({name},{posts:updatedarray})
+    let current = await model.findOne({ name })
+    if(current){
+        res.json(current.posts)
+    }
+    
 })
 module.exports = {
     router
